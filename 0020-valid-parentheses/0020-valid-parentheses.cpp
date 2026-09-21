fe9a3e6ac62abef4ed949113f23309d95134fcbb
Raksha Thakur <rakshathakur670@gmail.com>
2026-09-21 10:04:30 +0530

    Time: 2 ms (15.18%), Space: 8.9 MB (36.92%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+
+            // Opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+
+            // Closing brackets
+            else {
+                if (st.empty())
+                    return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
